340940f542d13ccf9965c5f8d4c35d09d19e38e8
RomanVReyes <142034738+RomanVReyes@users.noreply.github.com>
2024-02-23 21:14:36 -0800

    ppp

--- Personas.java
+++ Personas.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Persona {
     private String nombre;
     private int edad;
@@ -53,8 +55,8 @@ public class Persona {
         return direccion;
     }
 
-    public static void comer(String comida) {
-        System.out.println("Comiendo " + comida);
+    public void comer(String comida) {
+        System.out.println(nombre + " comiendo " + comida);
     }
 
     public void dormir(int horas) {
@@ -62,13 +64,43 @@ public class Persona {
     }
 
     public void hacerEjercicio(String ejercicio) {
-        System.out.println(nombre + " está haciendo ejercicio: " + ejercicio);
+        System.out.println(nombre + " está haciendo este ejercicio: " + ejercicio);
+    }
+
+    public void tomarBano(String momento) {
+        System.out.println(nombre + " está tomando un baño en " + momento);
     }
 
     public static void main(String[] args) {
-        Persona persona = new Persona("Juan", 30, "Masculino", "Ingeniero", "123 Calle Principal");
-        Persona.comer("Pizza");
-        persona.dormir(8);
-        persona.hacerEjercicio("Correr");
+        Random rand = new Random();
+
+        String[] comidas = {"Pizza", "Ensalada", "Hamburguesa", "Sushi", "Pasta"};
+        String[] ejercicios = {"Correr", "Nadar", "Hacer yoga", "Levantar pesas", "Caminar"};
+        String[] momentosBano = {"la mañana", "la tarde", "la noche"};
+
+        Persona[] personas = {
+            new Persona("Juan", 30, "Masculino", "Ingeniero", "123 Calle Principal"),
+            new Persona("María", 25, "Femenino", "Doctora", "456 Avenida Secundaria"),
+            new Persona("Pedro", 40, "Masculino", "Abogado", "789 Calle Principal")
+        };
+
+        for (Persona persona : personas) {
+            int actividadAleatoria = rand.nextInt(4);
+            switch (actividadAleatoria) {
+                case 0:
+                    persona.comer(comidas[rand.nextInt(comidas.length)]);
+                    break;
+                case 1:
+                    persona.dormir(rand.nextInt(8) + 1);
+                    break;
+                case 2:
+                    persona.hacerEjercicio(ejercicios[rand.nextInt(ejercicios.length)]);
+                    break;
+                case 3:
+                    persona.tomarBano(momentosBano[rand.nextInt(momentosBano.length)]);
+                    break;
+            }
+        }
     }
 }
+
